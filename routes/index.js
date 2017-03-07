cb1249d7c905a74fb28b25999d748a7da1f37c46
xmoyking <xmoyking@gmail.com>
2017-03-07 09:36:03 +0800

    添加自定义菜单

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var request = require('request');
 var crypto = require('crypto');
 var router = express.Router();
 
 var token = "WeChatOnNode"; // 可自定义,需要与微信平台设置的token一致！
+var appID = 'wx82869d3109acf2af';
+var APPSECRET = '57dde3521381a5d98de0f45840f1ca0f';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -24,7 +27,44 @@ router.get('/', function(req, res, next) {
 
     //3. 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
     if (code === signature) {
-        res.send(echostr)
+        //res.send(echostr);
+        request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + APPID + '&secret=' + APPSECRET, (err, res, body) => {
+            if (!err && res.errcode != undefined) {
+                console.log(res.access_token);
+                let jsonData = {
+                    "button": [{
+                            "type": "click",
+                            "name": "今日歌曲",
+                            "key": "V1001_TODAY_MUSIC"
+                        },
+                        {
+                            "name": "菜单",
+                            "sub_button": [{
+                                    "type": "view",
+                                    "name": "搜索",
+                                    "url": "http://www.soso.com/"
+                                },
+                                {
+                                    "type": "view",
+                                    "name": "视频",
+                                    "url": "http://v.qq.com/"
+                                },
+                                {
+                                    "type": "click",
+                                    "name": "赞一下我们",
+                                    "key": "V1001_GOOD"
+                                }
+                            ]
+                        }
+                    ]
+                };
+                request.post('https://api.weixin.qq.com/cgi-bin/menu/create?access_token=' + res.access_token, jsonData, (err, res, body) => {
+                    if (!err && res.errmsg === 'ok') {
+                        console.log('生成自定义菜单POST请求成功');
+                    }
+                });
+            }
+        });
     } else {
         res.send("index error");
     }
